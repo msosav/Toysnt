c3fa369f76a75f0a58eef0a84208b329ac0e55ca
cpalacior <cpalacior@eafit.edu.co>
2023-09-10 10:34:37 -0500

    migracion de review

---
+++ database/migrations/2023_09_09_021455_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment');
+            $table->string('rating');
+            $table->unsignedBigInteger('technique_id');
+            $table->foreign('technique_id')->references('id')->on('techniques');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
