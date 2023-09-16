0423209f45463f4788b7023c2e93b933eb54218c
cpalacior <cpalacior@eafit.edu.co>
2023-09-16 18:25:38 -0500

    arreglos menores de techniques

--- resources/views/technique/index.blade.php
+++ resources/views/technique/index.blade.php
@@ -1,16 +1,24 @@
 @extends('layouts.tabs')
 @section('title', $viewData['title'])
-@section('contentTabs')
-@foreach ($viewData['techniques'] as $technique)
-<div class="col-md-4 col-lg-3 mb-2">
-    <div class="card" style="width: 18rem;">
-        <img src="{{ $technique->getImage() }}" class="card-img-top" alt="{{ $technique->getModel() }}">
-        <div class="card-body">
-            <h5 class="card-title">
+@section('content_tabs')
+<div class="container row g-3 my-2 px-0">
+    @foreach ($viewData['techniques'] as $technique)
+    <div class="col-4 d-flex justify-content-start">
+        <div class="card me-2" style="width: 18rem;">
+            <img src="{{ URL::asset('storage/'.$technique->getImage()) }}" class="card-img-top" id="index-card-image">
+            <div class="card-body">
                 <a href="{{ route('technique.show', ['id'=> $technique->getId()]) }}">{{ $technique->getModel() }}</a>
-            </h5>
-        </div> 
-    </div> 
-</div> 
-@endforeach
+                <div class="row">
+                    <div class="col d-block">
+                        <h6 class="card-subtitle" id="card-price">${{ $technique->getPrice() }}</h6>
+                    </div>
+                    <div class="d-flex col justify-content-end">
+                        <a href="" class="btn btn-outline"><i class="fa-solid fa-cart-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+</div>
 @endsection
