7a947551894f19ba269948ae6351a770094d98e6
Miguel Sosa <miguelsosa2003@gmail.com>
2023-09-17 23:21:44 -0500

    Uniformidad en vista index

--- resources/views/technique/index.blade.php
+++ resources/views/technique/index.blade.php
@@ -2,18 +2,31 @@
 @section('title', $viewData['title'])
 @section('content_tabs')
 <div class="container row g-3 my-2 px-0">
+    @if (session('added'))
+    <div class="alert alert-success" role="alert">
+        {{ session('added') }}
+    </div>
+    @elseif (session('already_added'))
+    <div class="alert alert-danger" role="alert">
+        {{ session('already_added') }}
+    </div>
+    @endif
     @foreach ($viewData['techniques'] as $technique)
     <div class="col-4 d-flex justify-content-start">
         <div class="card me-2" style="width: 18rem;">
-            <img src="{{ URL::asset('storage/'.$technique->getImage()) }}" class="card-img-top" id="index-card-image">
+            <img src="{{ URL::asset('storage/'.$technique->getImage()) }}" class="card-img-top" alt="{{ $technique->getModel() }}" id="index-card-image">
             <div class="card-body">
-                <a href="{{ route('technique.show', ['id'=> $technique->getId()]) }}">{{ $technique->getModel() }}</a>
+                <h5 class="card-title"><a href="{{ route('technique.show', ['id'=> $technique['id']]) }}" id="card-title">{{ $technique->getModel() }}</a></h5>
                 <div class="row">
                     <div class="col d-block">
                         <h6 class="card-subtitle" id="card-price">${{ $technique->getPrice() }}</h6>
                     </div>
                     <div class="d-flex col justify-content-end">
-                        <a href="" class="btn btn-outline"><i class="fa-solid fa-cart-plus"></i></a>
+                        @guest
+                        <a href="{{ route('login') }}" class="btn btn-outline"><i class="fa-solid fa-cart-plus"></i></a>
+                        @else
+                        <a href="{{ route('cart.add', ['id'=> $technique->getId()]) }}" class="btn btn-outline"><i class="fa-solid fa-cart-plus"></i></a>
+                        @endguest
                     </div>
                 </div>
             </div>
