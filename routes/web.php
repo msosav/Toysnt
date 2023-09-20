24ef200f16d750dd33a5ecac301bb3ed755df89a
Miguel Sosa <miguelsosa2003@gmail.com>
2023-09-19 23:19:11 -0500

    Cambio nombre OrderController

--- routes/web.php
+++ routes/web.php
@@ -64,9 +64,8 @@ Route::get('/cart/removeToy/{id}', 'App\Http\Controllers\CartController@removeTo
 Route::get('/cart/removeTechnique/{id}', 'App\Http\Controllers\CartController@removeTechnique')->name('cart.removeTechnique');
 Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name('cart.removeAll');
 
-
 //Rutas de compra de juguetes
-Route::post('/purchase', 'App\Http\Controllers\PurchaseController@purchase')->name('purchase.purchase')->middleware('basic.users');
+Route::post('/purchase', 'App\Http\Controllers\OrderController@purchase')->name('purchase.purchase')->middleware('basic.users');
 
 //Rutas de Review.
 Route::get('/review/new', 'App\Http\Controllers\ReviewController@new')->name('review.new');
