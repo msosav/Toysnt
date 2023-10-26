217a23f4d97ec538da7196d05408136c159002e1
Miguel Sosa <85181687+msosav@users.noreply.github.com>
2023-10-25 21:18:40 -0500

    Create GCP.sh

---
+++ GCP.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian buster stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
