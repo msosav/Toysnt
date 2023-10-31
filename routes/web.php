ed39ffd09d92b81349b6c54754f99ae77e7becbb
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-31 12:29:08 -0500

    Corregir rutas de usuario

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+//Rutas de Auth.
+Auth::routes();
+
+//Rutas de Home.
 Route::get('/', 'App\Http\Controllers\ToyController@index')->name('toy.index')->middleware('landing.page');
 
 //Rutas de Technique.
