5211cf8d06f1e8d5e226dd53403c8dfaf69a97e8
Miguel Sosa <miguelsosa2003@gmail.com>
2023-10-31 10:49:04 -0500

    Corregir id unico para componentes de livewire

--- resources/views/livewire/cart/cart.blade.php
+++ resources/views/livewire/cart/cart.blade.php
@@ -24,12 +24,12 @@
                 <div class="card me-2 mb-4" style="width: 18rem;">
                     <img src="{{ URL::asset('storage/'.$toy->getImage()) }}" class="card-img-top" alt="{{ $toy->getName() }}" id="index-card-image">
                     <div class="card-body">
-                        <h5 class="card-title"><a wire:click="remove(''all', {{ $toy->getId() }}')" id="card-title">{{ $toy->getName() }}</a></h5>
+                        <h5 class="card-title"><a wire:click="remove('all', {{ $toy->getId() }})" id="card-title">{{ $toy->getName() }}</a></h5>
                         <div class="row">
                             <div class="col d-block">
                                 <h6 class="card-subtitle" id="card-price">${{ $toy->getPrice() }}</h6>
                             </div>
-                            @livewire('cart.cart-management', ['type' => 'toy', 'id' => $toy->getId()])
+                            @livewire('cart.cart-management', ['type' => 'toy', 'id' => $toy->getId()], key($toy->getId()))
                         </div>
                     </div>
                 </div>
@@ -47,7 +47,7 @@
                             <div class="col d-block">
                                 <h6 class="card-subtitle" id="card-price">${{ $technique->getPrice() }}</h6>
                             </div>
-                            @livewire('cart.cart-management', ['type' => 'technique', 'id' => $technique->getId()])
+                            @livewire('cart.cart-management', ['type' => 'technique', 'id' => $technique->getId(), key($technique->getId())])
                         </div>
                     </div>
                 </div>
