8d7b80a118f1d96a882ebb5502ef8803d0ad4fdc
EsteTruji <etrujilloc@eafit.edu.co>
2023-11-12 19:53:37 -0500

    Implement concrete class ImageGCPStorage

---
+++ app/Util/ImageGCPStorage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Util;
+
+use App\Interfaces\ImageStorage;
+use Illuminate\Http\Request;
+use Google\Cloud\Storage\StorageClient;
+
+class ImageGCPStorage implements ImageStorage
+{
+    public function store(Request $request, string $image_type, string $id): string
+    {
+        if ($request->hasFile($image_type)) {
+            $gcpkeyFile = env('GCP_KEY_FILE');
+            $storage = new StorageClient(['keyFile' => json_decode($gcpkeyFile, true)]);
+            $bucket = $storage->bucket(env('GCP_BUCKET'));
+            $gcpstoragePath = $image_type.'_'.$id.'.jpg';
+            $bucket->upload(file_get_contents($request->file($image_type)->getRealPath()), [
+            'name' => $gcpstoragePath,]);
+
+        }
+
+        return $image_type.'_'.$id.'.jpg';
+    }
+}
