d9bb337611921a3dbd1ccc96cbbf6d27458c6b47
EsteTruji <etrujilloc@eafit.edu.co>
2023-11-12 19:53:14 -0500

    Implement storage type selection for dependency inyection

--- app/Providers/ImageServiceProvider.php
+++ app/Providers/ImageServiceProvider.php
@@ -4,14 +4,19 @@ namespace App\Providers;
 
 use App\Interfaces\ImageStorage;
 use App\Util\ImageLocalStorage;
+use App\Util\ImageGCPStorage;
 use Illuminate\Support\ServiceProvider;
 
 class ImageServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(ImageStorage::class, function () {
-            return new ImageLocalStorage();
+        $this->app->bind(ImageStorage :: class, function ($app, $params) {
+            $storage = $params['storage'];
+            if ($storage == 'local') {
+            return new ImageLocalstorage();
+            } elseif ($storage == 'gcp') {
+            return new ImageGCPStorage();}
         });
     }
 }
