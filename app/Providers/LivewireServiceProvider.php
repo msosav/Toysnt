d2391f22f1f3ca16cb2aa11526242ba5cbc57164
msosav <msosav@users.noreply.github.com>
2023-11-14 15:37:29 +0000

    Fix linting

--- app/Providers/LivewireServiceProvider.php
+++ app/Providers/LivewireServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 
 class LivewireServiceProvider extends ServiceProvider
