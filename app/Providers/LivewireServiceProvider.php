e9e6518922bbfd88643754687a31579ad61f51fb
Miguel Sosa <miguelsosa2003@gmail.com>
2023-11-14 10:36:55 -0500

    Implement livewire provider

---
+++ app/Providers/LivewireServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
+
+class LivewireServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        Livewire::setScriptRoute(function ($handle) {
+            return Route::get('/public/livewire/livewire.js', $handle);
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ImageServiceProvider::class,
+        App\Providers\LivewireServiceProvider::class,
     ])->toArray(),
 
     /*
